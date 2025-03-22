b7f74b39730efdd57cb6cfb057343c06f9571997
KJoshO0611 <jeslagam@gmail.com>
2025-03-22 16:17:47 +0800

    Added support for guild specific avatars

--- utils/cairo_image_generator.py
+++ utils/cairo_image_generator.py
@@ -602,8 +602,17 @@ async def generate_level_card(member, level, xp, xp_needed):
     # Format user ID with # prefix
     user_id_display = f"#{user_id[-4:]}"
     
-    # Download avatar
-    avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
+    avatar_url = None
+    # Try to get server-specific (guild) avatar first
+    if hasattr(member, 'guild_avatar') and member.guild_avatar:
+        avatar_url = member.guild_avatar.url
+    # Then fall back to global avatar
+    elif member.avatar:
+        avatar_url = member.avatar.url
+    # Finally, use default avatar as last resort
+    else:
+        avatar_url = member.default_avatar.url
+
     async with aiohttp.ClientSession() as session:
         async with session.get(avatar_url) as resp:
             if resp.status == 200:
